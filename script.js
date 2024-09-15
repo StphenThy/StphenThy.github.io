f235f014022b32280baf71704de500f34bd73fff
StphenThy <stohoy_230000001289@uic.edu.ph>
2024-09-15 12:23:27 +0800

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const showImageButton = document.getElementById('showImageButton');
-    const imageContainer = document.getElementById('imageContainer');
 
-    showImageButton.addEventListener('click', () => {
-        console.log('Button clicked'); // Debugging message
-        imageContainer.style.display = 'block'; // Show the image
-        showImageButton.style.display = 'none'; // Option to hide the button after clicking
-    });
-});
